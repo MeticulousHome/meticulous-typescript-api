987120638a10ab59410510f7e45d616ec14ce955
Mimoja <mimoja@meticuloushome.com>
2024-04-02 16:29:23 +0200

    feat(axios): Implement base handlers to fetch the json base API endpoints
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

---
+++ src/types.ts
@@ -0,0 +1,80 @@
+export type ActionType = 'start' | 'stop' | 'reset' | 'tare' | 'calibration';
+
+export interface ActionResponse {
+  action?: string;
+  allowed_actions?: string[];
+}
+
+export interface FileListing {
+  name: string;
+  url: string;
+}
+
+export interface Notification {
+  id: string;
+  message: string;
+  image?: string;
+  response_options?: string[];
+  timestamp: string;
+}
+
+export interface AcknowledgeNotificationRequest {
+  id: string;
+  response: string;
+}
+
+export interface ProfileIdent {
+  name: string;
+  id: string;
+}
+
+export interface Settings {
+  // Setting name and value
+  [key: string]: string;
+}
+
+export type APMode = 'AP' | 'CLIENT';
+
+export interface WiFiConfig {
+  mode: APMode;
+  apName: string;
+  apPassword: string;
+}
+
+export interface WiFiConnectRequest {
+  ssid: string;
+  password: string;
+}
+
+export interface WifiSystemStatus {
+  connected: boolean;
+  connection_name: string;
+  gateway: string;
+  routes: string[];
+  ips: string[];
+  dns: string[];
+  mac: string;
+  hostname: string;
+  domains: string[];
+}
+
+export interface WifiStatus {
+  config: WiFiConfig;
+  status: WifiSystemStatus;
+}
+
+export interface WiFiNetwork {
+  ssid: string;
+  signal: number;
+  rate: number;
+  in_use: boolean;
+}
+
+export interface APIError {
+  error: string;
+  description: string;
+  data?: object;
+}
+
+// Placeholder until we habe the profile definition in a library
+export type Profile = object;
