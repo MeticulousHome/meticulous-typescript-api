e17b7f0750a72eb6f7e7e0b6555b7fe7d4b99564
Mimoja <mimoja@meticuloushome.com>
2024-09-24 15:11:07 -0600

    action: add scale_master_calibration
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 import { Profile } from '@meticulous-home/espresso-profile';
 
-export type ActionType = 'start' | 'stop' | 'reset' | 'tare' | 'calibration';
+export type ActionType = 'start' | 'stop' | 'reset' | 'tare' | 'calibration' | 'scale_master_calibration';
 
 export interface ActionResponse {
   action?: string;
