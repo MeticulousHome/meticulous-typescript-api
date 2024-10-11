47b0250812cfe81abb060b497bb7fdeb359b3753
Angel-Padilla <angel.p.lopez@outlook.com>
2024-10-10 18:37:04 -0600

    refactor(index.ts): change version of api from hardcoded text to variable substitution

--- src/index.ts
+++ src/index.ts
@@ -47,6 +47,7 @@ export default class Api {
   private socket: Socket | undefined = undefined;
 
   private serverURL: string;
+  private version: string = 'v1'
 
   constructor(
     private options?: MachineDataClientOptions,
@@ -103,60 +104,60 @@ export default class Api {
   async executeAction(
     action: ActionType
   ): Promise<AxiosResponse<ActionResponse | APIError>> {
-    return this.axiosInstance.get(`/api/v1/action/${action}`);
+    return this.axiosInstance.get(`/api/${this.version}/action/${action}`);
   }
 
   async listProfiles(): Promise<AxiosResponse<ProfileIdent[] | APIError>> {
-    return this.axiosInstance.get('/api/v1/profile/list');
+    return this.axiosInstance.get(`/api/${this.version}/profile/list`);
   }
 
   async fetchAllProfiles(): Promise<AxiosResponse<Profile[] | APIError>> {
-    return this.axiosInstance.get('/api/v1/profile/list?full=true');
+    return this.axiosInstance.get(`/api/${this.version}/profile/list?full=true`);
   }
 
   async saveProfile(
     data: Profile
   ): Promise<AxiosResponse<ProfileIdent | APIError>> {
-    return this.axiosInstance.post('/api/v1/profile/save', data);
+    return this.axiosInstance.post(`/api/${this.version}/profile/save`, data);
   }
 
   async loadProfileFromJSON(
     data: Profile
   ): Promise<AxiosResponse<ProfileIdent | APIError>> {
-    return this.axiosInstance.post('/api/v1/profile/load', data);
+    return this.axiosInstance.post(`/api/${this.version}/profile/load`, data);
   }
 
   async loadProfileByID(
     id: string
   ): Promise<AxiosResponse<ProfileIdent | APIError>> {
-    return this.axiosInstance.get(`/api/v1/profile/load/${id.toString()}`);
+    return this.axiosInstance.get(`/api/${this.version}/profile/load/${id.toString()}`);
   }
 
   async getProfile(
     profileId: string
   ): Promise<AxiosResponse<Profile | APIError>> {
-    return this.axiosInstance.get(`/api/v1/profile/get/${profileId}`);
+    return this.axiosInstance.get(`/api/${this.version}/profile/get/${profileId}`);
   }
 
   async deleteProfile(
     profileId: string
   ): Promise<AxiosResponse<ProfileIdent | APIError>> {
-    return this.axiosInstance.delete(`/api/v1/profile/delete/${profileId}`);
+    return this.axiosInstance.delete(`/api/${this.version}/profile/delete/${profileId}`);
   }
 
   async getLastProfile(): Promise<AxiosResponse<LastProfileIdent | APIError>> {
-    return this.axiosInstance.get('/api/v1/profile/last');
+    return this.axiosInstance.get(`/api/${this.version}/profile/last`);
   }
 
   async getProfileDefaultImages(): Promise<AxiosResponse<string[] | APIError>> {
-    return this.axiosInstance.get('/api/v1/profile/image');
+    return this.axiosInstance.get(`/api/${this.version}/profile/image`);
   }
 
   getProfileImageUrl(image: string): string {
     if (image.startsWith('data:')) {
       return image;
     }
-    const url = '/api/v1/profile/image/';
+    const url = `/api/${this.version}/profile/image/`;
     if (!image.startsWith(url)) {
       image = url + image;
     }
@@ -177,33 +178,33 @@ export default class Api {
     acknowledged: boolean
   ): Promise<AxiosResponse<Notification[] | APIError>> {
     return this.axiosInstance.get(
-      `/api/v1/notifications?acknowledged=${acknowledged}`
+      `/api/${this.version}/notifications?acknowledged=${acknowledged}`
     );
   }
 
   async acknowledgeNotification(
     data: AcknowledgeNotificationRequest
   ): Promise<AxiosResponse<void | APIError>> {
-    return this.axiosInstance.post('/api/v1/notifications/acknowledge', data);
+    return this.axiosInstance.post(`/api/${this.version}/notifications/acknowledge`, data);
   }
 
   async getSettings(
     settingName?: string
   ): Promise<AxiosResponse<Settings | APIError>> {
-    const url = '/api/v1/settings' + (settingName ? `/${settingName}` : '');
+    const url = `/api/${this.version}/settings` + (settingName ? `/${settingName}` : '');
     return this.axiosInstance.get(url);
   }
 
   async updateSetting(
     setting: Partial<Settings>
   ): Promise<AxiosResponse<Settings | APIError>> {
-    return this.axiosInstance.post('/api/v1/settings', setting);
+    return this.axiosInstance.post(`/api/${this.version}/settings`, setting);
   }
 
   async updateFirmware(
     formData: FormData
   ): Promise<AxiosResponse<void | APIError>> {
-    return this.axiosInstance.post('/api/v1/update/firmware', formData, {
+    return this.axiosInstance.post(`/api/${this.version}/update/firmware`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data'
       }
@@ -211,18 +212,18 @@ export default class Api {
   }
 
   async getWiFiStatus(): Promise<AxiosResponse<WifiStatus | APIError>> {
-    return this.axiosInstance.get('/api/v1/wifi/config');
+    return this.axiosInstance.get(`/api/${this.version}/wifi/config`);
   }
 
   async setWiFiConfig(
     data: Partial<WiFiConfig>
   ): Promise<AxiosResponse<WiFiConfig | APIError>> {
-    return this.axiosInstance.post('/api/v1/wifi/config', data);
+    return this.axiosInstance.post(`/api/${this.version}/wifi/config`, data);
   }
 
   async getWiFiQR(): Promise<AxiosResponse<Blob>> {
     const response = await this.axiosInstance.get(
-      '/api/v1/wifi/config/qr.png',
+      `/api/${this.version}/wifi/config/qr.png`,
       {
         responseType: 'blob'
       }
@@ -232,19 +233,19 @@ export default class Api {
 
   getWiFiQRURL(): string {
     return new URL(
-      '/api/v1/wifi/config/qr.png',
+      `/api/${this.version}/wifi/config/qr.png`,
       this.axiosInstance.getUri()
     ).toString();
   }
 
   async listAvailableWiFi(): Promise<AxiosResponse<WiFiNetwork[] | APIError>> {
-    return this.axiosInstance.get('/api/v1/wifi/list');
+    return this.axiosInstance.get(`/api/${this.version}/wifi/list`);
   }
 
   async connectToWiFi(
     data: WiFiCredentials
   ): Promise<AxiosResponse<void | APIError>> {
-    return this.axiosInstance.post('/api/v1/wifi/connect', data);
+    return this.axiosInstance.post(`/api/${this.version}/wifi/connect`, data);
   }
 
   async deleteWifi({
@@ -252,47 +253,47 @@ export default class Api {
   }: {
     ssid: string;
   }): Promise<AxiosResponse<void | APIError>> {
-    return this.axiosInstance.post('/api/v1/wifi/delete', { ssid });
+    return this.axiosInstance.post(`/api/${this.version}/wifi/delete`, { ssid });
   }
 
   async playSound(sound: string): Promise<AxiosResponse<void | APIError>> {
-    return this.axiosInstance.get(`/api/v1/sounds/play/${sound}`);
+    return this.axiosInstance.get(`/api/${this.version}/sounds/play/${sound}`);
   }
 
   async listSounds(): Promise<AxiosResponse<string[] | APIError>> {
-    return this.axiosInstance.get('/api/v1/sounds/list');
+    return this.axiosInstance.get(`/api/${this.version}/sounds/list`);
   }
 
   async listSoundThemes(): Promise<AxiosResponse<string[] | APIError>> {
-    return this.axiosInstance.get('/api/v1/sounds/theme/list');
+    return this.axiosInstance.get(`/api/${this.version}/sounds/theme/list`);
   }
 
   async getSoundTheme(): Promise<AxiosResponse<string | APIError>> {
-    return this.axiosInstance.get('/api/v1/sounds/theme/get');
+    return this.axiosInstance.get(`/api/${this.version}/sounds/theme/get`);
   }
 
   async setSoundTheme(theme: string): Promise<AxiosResponse<void | APIError>> {
-    return this.axiosInstance.post(`/api/v1/sounds/theme/set/${theme}`);
+    return this.axiosInstance.post(`/api/${this.version}/sounds/theme/set/${theme}`);
   }
 
   async getDeviceInfo(): Promise<AxiosResponse<DeviceInfo | APIError>> {
-    return this.axiosInstance.get('/api/v1/machine');
+    return this.axiosInstance.get(`/api/${this.version}/machine`);
   }
 
   async getDefaultProfiles(): Promise<AxiosResponse<Profile[] | APIError>> {
-    return this.axiosInstance.get('/api/v1/profile/defaults');
+    return this.axiosInstance.get(`/api/${this.version}/profile/defaults`);
   }
 
   async getHistoryShortListing(): Promise<
     AxiosResponse<HistoryListingResponse>
   > {
-    return this.axiosInstance.get('/api/v1/history');
+    return this.axiosInstance.get(`/api/${this.version}/history`);
   }
 
   async searchHistory(
     query: Partial<HistoryQueryParams>
   ): Promise<AxiosResponse<HistoryResponse>> {
-    return this.axiosInstance.post('/api/v1/history', {
+    return this.axiosInstance.post(`/api/${this.version}/history`, {
       params: { query }
     });
   }
@@ -300,22 +301,22 @@ export default class Api {
   async searchHistoricalProfiles(
     query: string
   ): Promise<AxiosResponse<HistoryListingResponse>> {
-    return this.axiosInstance.get('/api/v1/history/search?query=' + query);
+    return this.axiosInstance.get(`/api/${this.version}/history/search?query=` + query);
   }
 
   async getCurrentShot(): Promise<AxiosResponse<HistoryEntry | null>> {
-    return this.axiosInstance.get('/api/v1/history/current');
+    return this.axiosInstance.get(`/api/${this.version}/history/current`);
   }
 
   async getLastShot(): Promise<AxiosResponse<HistoryEntry | null>> {
-    return this.axiosInstance.get('/api/v1/history/last');
+    return this.axiosInstance.get(`/api/${this.version}/history/last`);
   }
 
   async getHistoryStatistics(): Promise<AxiosResponse<HistoryStats>> {
-    return this.axiosInstance.get('/api/v1/history/stats');
+    return this.axiosInstance.get(`/api/${this.version}/history/stats`);
   }
 
   async getOSStatus(): Promise<AxiosResponse<HistoryStats>> {
-    return this.axiosInstance.get('/api/v1/machine/OS_update_status');
+    return this.axiosInstance.get(`/api/${this.version}/machine/OS_update_status`);
   }
 }
