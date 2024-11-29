a968402eb6ac69a824cd69e94d31908a65e456b7
Mimoja <git@mimoja.de>
2024-11-29 16:21:06 +0100

    Revert "settings: add timezone set/get endpoint"
    This reverts commit 30e261160f1e0b36172786f29aceb066d791e55b.

--- src/index.ts
+++ src/index.ts
@@ -209,20 +209,6 @@ export default class Api {
     return this.axiosInstance.get(url);
   }
 
-  async getTimezones(): Promise<AxiosResponse<Settings | APIError>> {
-    const url = `/api/${this.version}/settings/timezones`;
-    return this.axiosInstance.get(url);
-  }
-
-  async setTimezone(
-    timeZone: string
-  ): Promise<AxiosResponse<Settings | APIError>> {
-    return this.axiosInstance.post(
-      `/api/${this.version}/settings/timezones`,
-      timeZone
-    );
-  }
-
   async updateSetting(
     setting: Partial<Settings>
   ): Promise<AxiosResponse<Settings | APIError>> {
